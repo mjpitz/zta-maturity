2c15321a863d826fbf1910a6a14b18695d9d1eb6
mya <j.mya.pitz@gmail.com>
2026-05-03 04:36:00 -0500

    fix: point astro at the right location

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://mjpitz.github.io/zta-maturity',
+    base: '/zta-maturity',
+});
